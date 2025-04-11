e6a34533a39934c9f6d9a695e1ddfe40b996d89a
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2025-04-11 18:45:33 +0900

    [Deploy]: package dependencies 추가

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "NavigationManager",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NavigationManager",
             targets: ["NavigationManager"]),
+    ],
+    dependencies: [
+       .package(url: "https://github.com/your_username/NavigationManager.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
